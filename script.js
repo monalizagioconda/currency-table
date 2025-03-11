c7a2d17119ce3d201eb44c8bda98af490f399fb6
Dorota Stefanik <dorota.stefanik@gmail.com>
2025-03-11 15:17:30 +0100

    Split code into functions.

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-async function startApp() {
-  const apiUrl = "https://api.nbp.pl/api/exchangerates/tables/a?format=json"; // kod JSON zwracany przez serwer
-  const response = await fetch(apiUrl); // chcemy poczekać aż funkcja fetch sie wykona
+async function fetchData() {
+  const apiUrl = "https://api.nbp.pl/api/exchangerates/tables/a?format=json";
+  const response = await fetch(apiUrl);
   const data = await response.json();
-  console.log(data);
 
-  processData(data[0]); // funkcja wykona się dopiero po ściągnięciu data
+  return data[0];
 }
-startApp();
 
-function processData(obj) {
+async function main() {
+  const obj = await fetchData();
   const tableNum = obj.no;
   const ratesArr = obj.rates;
 
   document.getElementById("date").innerHTML = tableNum;
 
-  const dataTableDiv = document.getElementById("data-table"); // zapamiętana referencja do obiektu
-
-  ratesArr.forEach(element => {
-    const code = element.code; // USD
-    const currency = element.currency; // dolar amerykański
-    const price = element.mid; // 4.1263
-
-    addRateContent(code, currency, price, dataTableDiv);
-  });
+  ratesArr.forEach(renderRate);
 }
 
+function renderRate(element) {
+  const code = element.code;
+  const currency = element.currency;
+  const price = element.mid;
+
+  addRateContent(code, currency, price, dataTableDiv);
+}
+
+const dataTableDiv = document.getElementById("data-table");
+
 function addRateContent(code, currency, price, dataTableDiv) {
-  const el = document.createElement("div"); // dodajemy nowy element div, który narazie jest w pamięci
-  el.classList.add("rate"); // przypisujemy mu klasę 'rate'
+  const el = document.createElement("div");
+  el.classList.add("rate");
 
   el.innerHTML = `
     <div> ${code} </div>
     <div> ${currency} </div>
     <div> ${price} zł </div>
-  `; // !!! w innerHTML możemy pisać kod HTML
+  `;
 
-  dataTableDiv.append(el); // dodanie el do dataTableDiv. Append nie usunie poprzednich el. tylko doda nowy
+  dataTableDiv.appendChild(el);
 }
+
+main();
